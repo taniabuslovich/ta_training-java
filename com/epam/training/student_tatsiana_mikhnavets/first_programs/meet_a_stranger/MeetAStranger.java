c8f82c2cac984e5c3d060628f7d6740fe84077ae
Tania Buslovich <taniabuslovich@mail.ru>
2023-03-30 14:56:21 +0300

    first tasks

---
+++ com/epam/training/student_tatsiana_mikhnavets/first_programs/meet_a_stranger/MeetAStranger.java
@@ -0,0 +1,11 @@
+package com.epam.training.student_tatsiana_mikhnavets.first_programs.meet_a_stranger;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Hello, " + input);
+    }
+}
---
+++ com/epam/training/student_tatsiana_mikhnavets/first_programs/meet_autocode/MeetAutocode.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_tatsiana_mikhnavets.first_programs.meet_autocode;
+
+public class MeetAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+    }
+}
