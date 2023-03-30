51d2968232c18081fe5af86b0486e36b0172d1c0
Tania Buslovich <taniabuslovich@mail.ru>
2023-03-30 15:12:37 +0300

    first tasks

---
+++ com/epam/training/student_tatsiana_mikhnavets/conditions_and_cycles/meet_strangers/MeetStrangers.java
@@ -0,0 +1,23 @@
+package com.epam.training.student_tatsiana_mikhnavets.conditions_and_cycles.meet_strangers;
+
+import java.util.Scanner;
+
+public class MeetStrangers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        if (input == 0) {
+            System.out.println("Oh, it looks like there is no one here");
+        } else if (input < 0) {
+            System.out.println("Seriously? Why so negative?");
+        } else {
+            for (int i = 0; i <= input; i++) {
+                String str = (scanner.nextLine());
+                if (str.isEmpty()) continue;
+                System.out.println("Hello, " + str);
+            }
+        }
+    }
+}
