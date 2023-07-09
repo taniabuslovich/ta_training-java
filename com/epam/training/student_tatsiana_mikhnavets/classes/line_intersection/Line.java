4121cd0340bd48604298fed62d1bf992fe466bc6
Tania Buslovich <taniabuslovich@mail.ru>
2023-07-09 16:29:33 +0300

    first tasks

---
+++ com/epam/training/student_tatsiana_mikhnavets/classes/line_intersection/Line.java
@@ -0,0 +1,26 @@
+package com.epam.training.student_tatsiana_mikhnavets.classes.line_intersection;
+
+public class Line {
+    public int k;
+    public int b;
+
+    public Line(int k, int b) {
+        this.k=k;
+        this.b=b;
+
+    }
+
+    public Point intersection(Line other) {
+        int x;
+        int y;
+        if (k!= other.k) {
+            x=(other.b-b)/(k- other.k);
+            y=k*x+b;
+        }else return null;
+        return new Point(x,y);
+
+    }
+
+}
+
+
