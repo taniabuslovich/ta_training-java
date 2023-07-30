2e3029b15cfca073f7ac3897ab86857d9739dc19
Tania Buslovich <taniabuslovich@mail.ru>
2023-07-31 00:56:27 +0300

    first tasks

---
+++ com/epam/training/student_tatsiana_mikhnavets/classes/triangle/Triangle.java
@@ -0,0 +1,34 @@
+package com.epam.training.student_tatsiana_mikhnavets.classes.triangle;
+
+import static java.lang.Math.*;
+
+public class Triangle {
+    Point pointA;
+    Point pointB;
+    Point pointC;
+
+    public Triangle(Point a, Point b, Point c) {
+        pointA = a;
+        pointB =b;
+        pointC = c;
+        double doubleAB = sqrt(pow((pointB.getX() - pointA.getX()), 2) + pow((pointB.getY() - pointA.getY()), 2));
+        double doubleBC = sqrt(pow((pointC.getX()- pointB.getX()),2)+pow((pointC.getY()-pointB.getY()),2));
+        double doubleCA = sqrt(pow((pointA.getX()-pointC.getX()),2)+pow((pointA.getY()- pointC.getY()),2));
+        if (doubleAB+doubleBC > doubleCA && doubleBC+doubleCA>doubleAB && doubleCA+doubleAB>doubleBC) {
+
+
+        }
+        else throw new IllegalArgumentException();
+    }
+
+
+    public double area() {
+        return abs(((pointB.getX()-pointA.getX())*((pointC.getY())-pointA.getY())-(pointC.getX()-pointA.getX())*(pointB.getY()-pointA.getY()))/2);
+    }
+
+    public Point centroid(){
+        return new Point ((pointA.getX()+pointB.getX()+pointC.getX())/3,(pointA.getY()+pointB.getY()+ pointC.getY())/3);
+    }
+
+}
+
