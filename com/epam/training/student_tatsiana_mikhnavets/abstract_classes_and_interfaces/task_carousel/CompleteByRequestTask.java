8e92c57f1ae3d715740700709b3114cdba53457f
Tania Buslovich <taniabuslovich@mail.ru>
2023-09-26 17:14:11 +0300

    first tasks

---
+++ com/epam/training/student_tatsiana_mikhnavets/abstract_classes_and_interfaces/task_carousel/CompleteByRequestTask.java
@@ -0,0 +1,22 @@
+package com.epam.training.student_tatsiana_mikhnavets.abstract_classes_and_interfaces.task_carousel;
+
+public class CompleteByRequestTask implements Task {
+    private boolean isFinished = false;
+    private boolean complete = false;
+
+    @Override
+    public void execute() {
+        if (complete) isFinished = true;
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return isFinished;
+    }
+
+    public void complete() {
+
+        complete = true;
+    }
+}
