f6bf092c115b0e976ae9953de92b762156268845
Tania Buslovich <taniabuslovich@mail.ru>
2023-10-02 11:58:09 +0300

    first tasks

---
+++ com/epam/training/student_tatsiana_mikhnavets/nested_classes/arithmetic_expressions/Expression.java
@@ -0,0 +1,6 @@
+package com.epam.training.student_tatsiana_mikhnavets.nested_classes.arithmetic_expressions;
+
+public interface Expression {
+    int evaluate();
+    String toExpressionString();
+}
