987fc3f8e49dd3a7d76a5c0fe3bb4d670de3f5b0
Tania Buslovich <taniabuslovich@mail.ru>
2023-10-05 17:20:31 +0300

    first tasks

---
+++ com/epam/training/student_tatsiana_mikhnavets/work_with_strings/validations_color_code/ColorCodeValidation.java
@@ -0,0 +1,15 @@
+package com.epam.training.student_tatsiana_mikhnavets.work_with_strings.validations_color_code;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ColorCodeValidation {
+    public static boolean validateColorCode(String color) {
+        if (color == null) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("#[0-9a-f]{3}|#[0-9a-f]{6}", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(color);
+        return matcher.matches();
+    }
+}
